0dc39a35f9dd2187f19fd5790c55b3d7ea3caf03
Sushant Ramkrushna Telgote <sushanttelgote4@gmail.com>
2025-01-27 21:33:25 +0530

    Create 1DAY.N-Triangle2.cpp

---
+++ 1DAY.N-Triangle2.cpp
@@ -0,0 +1,29 @@
+Input: ‘N’ = 3
+
+Output: 
+1
+2 2 
+3 3 3
+
+Program : 
+
+void triangle(int n) {
+	// Write your code here
+  for(int i = 1; i <= n ; i++){
+
+    for( int j =1;j<=i;j++){
+
+		cout << i << " ";
+	}
+    cout << endl;
+
+  }
+
+}  
+
+
+
+explain : Only Print the rows number  
+  
+  
+  
